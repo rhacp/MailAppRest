62e7f94e58d61b42789934a0ca8440ab3412a778
Andrei <mirceanghel98@gmail.com>
2023-11-19 17:14:53 +0200

    First Commit

---
+++ src/main/java/com/team/MailRestVariant/MailRestVariantApplication.java
@@ -0,0 +1,13 @@
+package com.team.MailRestVariant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailRestVariantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailRestVariantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/MailRestVariant/MailRestVariantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.MailRestVariant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailRestVariantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
